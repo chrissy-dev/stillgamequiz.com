c57000736aee641e2794adc998aae45036c836a2
Chrissy Collins <chrissy@Chrissys-MacBook-Pro.local>
2016-11-03 22:09:25 +0000

    fixed

---
+++ _scripts/questions.js
@@ -0,0 +1,205 @@
+var data = {
+    "started": false,
+    "settings": {
+        "index": 0,
+        "total": 0,
+        "show_answer": false
+    },
+    "current": {
+        "question": "",
+        "answer": "",
+    },
+    "question_bank": [{
+        "id": 1,
+        "question": "In season 1 episode 1 (Flittin') Tams grandson Joe is fighting in a boxing match. What is the full name of the boy he is fighting?",
+        "explanation": null,
+        "answer": "Barry Drummond",
+        "season": "1"
+    }, {
+        "id": 2,
+        "question": "In the episode \"Faimly\" Jack and Victor arrive at central station very early. What time did they arrive?",
+        "explanation": "",
+        "answer": "10:00",
+        "season": "1"
+    }, {
+        "id": 3,
+        "question": "How much did Winston win at Stevie’s bookie before Stevie did the off?",
+        "explanation": null,
+        "answer": "£32,500",
+        "season": "3"
+    }, {
+        "id": 4,
+        "question": "What was controversial Tam's prize for winning the card game in the episode \"Cairds\"?",
+        "explanation": "",
+        "answer": "Auld Joes buggy",
+        "season": "3"
+    }, {
+        "id": 5,
+        "question": "In the episode \"Dial-A-Bus\" Boaby cycles round the countryside and stops at a bar. The barman asks what he does for a living. What does he say he does for a living?",
+        "explanation": null,
+        "answer": "Lawyer",
+        "season": "4"
+    }, {
+        "id": 6,
+        "question": "What was the name of the pizza shop girl that Boabby was trying to fire into?",
+        "explanation": null,
+        "answer": "Stacey",
+        "season": "6"
+    }, {
+        "id": 7,
+        "question": "There's a guess the name of the dog competition going on, and the prize is a robot dog. What name does Jack choose?",
+        "explanation": "There's only two names left (Matthew and Richard), Jack chooses \"Richard\".",
+        "answer": "Richard (Dick)",
+        "season": "2"
+    }, {
+        "id": 8,
+        "question": "Who do Jack and Victor dress as for The Clansman's fancy dress night?",
+        "explanation": "They both forget as they walk in and Boaby said \"nae costume nae pint\" so they went as each other.",
+        "answer": "Each other",
+        "season": "4"
+    }, {
+        "id": 9,
+        "question": "Innes isn't allowed to drink Midori. What was the name of the shop Isa said he'd get it from?",
+        "explanation": null,
+        "answer": "Food Fayre",
+        "season": "3"
+    }, {
+        "id": 10,
+        "question": "Winston is asked to look after his grandson while his daughter and husband go to Blackpool. What is the name of Winstons grandson?",
+        "explanation": "When Winston opens the door to them he says both their names",
+        "answer": "Thomas",
+        "season": "4"
+    }, {
+        "id": 11,
+        "question": "In \"Wireless\" Jack and Victor are hospital DJs. They are asked to play a particular song from a dying patient and they have to get the record off of Isa. What was the name of that song and who sang it?",
+        "explanation": null,
+        "answer": "Nelson Eadie - Rose Marie",
+        "season": "4"
+    }, {
+        "id": 12,
+        "question": "Jack takes Victor on a sea faring adventure down the River Kelvin for his birthday. What age was Victor turning?",
+        "explanation": null,
+        "answer": "75",
+        "season": "5"
+    }, {
+        "id": 13,
+        "question": "Jack and Victor are chauffeurs for the day in Hugie Cassies car. They pick up Fergies girlfriend Sinead and her dad does a scramble. How much did he throw out?",
+        "explanation": "Sineads asks her da how much he flung oot and he says \"3 pound and a power caird\" ",
+        "answer": "3 pound and a power caird",
+        "season": "5"
+    }, {
+        "id": 14,
+        "question": "When Jack and Victor go back to school to study medical profiecency. How much was a pint in their student bar?",
+        "explanation": "The wee receptionist wummin tells them",
+        "answer": "75p",
+        "season": "3"
+    }, {
+        "id": 15,
+        "question": "Who was Wullie Napiers next door neighbour?",
+        "explanation": "Jack and Victor had to use his phone to call his boy",
+        "answer": "Manky Frankie",
+        "season": "1"
+    }, {
+        "id": 16,
+        "question": "True or false does Navid have any children?",
+        "explanation": "His daughter has a wedding in season 1",
+        "answer": "True",
+        "season": "1"
+    }, {
+        "id": 17,
+        "question": "How much money does Winston win from a can of cat food?",
+        "explanation": null,
+        "answer": "£300",
+        "season": "1"
+    }, {
+        "id": 18,
+        "question": "Winston gets attacked by a dog while shouting abuse at two neds, what was the dog called? ",
+        "explanation": "When the devil dug jumps off the roof of the flats, his owner shouts his name",
+        "answer": "Kiazer",
+        "season": "2"
+    }, {
+        "id": 19,
+        "question": "What does Victor bring with him when he goes up to visit Bert Finlay?",
+        "explanation": "He dusney like the mint wans he only likes the toffee wans",
+        "answer": "Mint Yo-Yos",
+        "season": "2"
+    }, {
+        "id": 20,
+        "question": "How does Winston get revenge on Tam for embarssing him in front of Frances in the clansman?",
+        "explanation": null,
+        "answer": "Shites in his book",
+        "season": "2"
+    }, {
+        "id": 21,
+        "question": "When Victor gets a car, how much does he pay for insurance?",
+        "explanation": null,
+        "answer": "£300",
+        "season": "2"
+    }, {
+        "id": 22,
+        "question": "What was the name of the street Tam was brought up in?",
+        "explanation": "Winston asks Jack and Victor the question when he's preparing his best man speech",
+        "answer": "Brogen Street",
+        "season": "5"
+    }, {
+        "id": 23,
+        "question": "What year did Pete the jakey invent the beefy bake?",
+        "explanation": "",
+        "answer": "1979",
+        "season": "6"
+    }, {
+        "id": 24,
+        "question": "What day of the week did Pete the jakey invent the beefy bake?",
+        "explanation": "Ovens were off on a Tuesday cause that's when they done the cleaning",
+        "answer": "Tuesday",
+        "season": "6"
+    }, {
+        "id": 25,
+        "question": "What was the number of the tram that knocked Frank McCallum down?",
+        "explanation": "Jack tells Victor when they are in the transport meuseum",
+        "answer": "46",
+        "season": "2"
+    }, {
+        "id": 26,
+        "question": "How much did Victor have to pay for a meal after their waiter was sacked?",
+        "explanation": "",
+        "answer": "£140",
+        "season": "6"
+    }, {
+        "id": 27,
+        "question": "What was the name of the waiter that got sacked while serving Jack and Victor",
+        "explanation": null,
+        "answer": "Norman",
+        "season": "6"
+    }, {
+        "id": 28,
+        "question": "Who owns a house on the island Hotenanny?",
+        "explanation": null,
+        "answer": "Auld Joe",
+        "season": "Special"
+    }, {
+        "id": 29,
+        "question": "When Victor gets a car what make was the car?",
+        "explanation": null,
+        "answer": "Skoda",
+        "season": "2"
+    }, {
+        "id": 30,
+        "question": "In Hot Seat how do Tam and Winston get early access to the park?",
+        "explanation": null,
+        "answer": "Ice Cream van",
+        "season": "6"
+    }, {
+        "id": 31,
+        "question": "Where does Archie the hermet hide all his social security money?",
+        "explanation": null,
+        "answer": "In his couch",
+        "season": "4"
+    }, {
+        "id": 32,
+        "question": "Just before Peggy and Winston start brawlin in the butchers, how many chigot chops does Peggy buy?",
+        "explanation": null,
+        "answer": "6",
+        "season": "1"
+    }]
+};
